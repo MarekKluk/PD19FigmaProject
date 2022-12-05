06d56dfb1b5c9aa2da87c53fdb96418e98c5f8d1
maroosek <kluk.marek1@gmail.com>
2022-12-05 14:24:48 +0100

    Changed creating fake data project to TS version

--- build/index.js
+++ build/index.js
@@ -1,49 +1,46 @@
-import './styles.css';
-import { faker } from '@faker-js/faker/locale/pl';
-
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+require('../build/styles.css');
+const pl_1 = require('@faker-js/faker/locale/pl');
 let venueArray = [];
 let venueDetailsArray = [];
 let usedIdsArray = [];
 let generatedLocalizations = [];
 let addressesCache = [];
-
-//faker unique
 function generateUniqueId() {
-  let randomNumber = faker.datatype.number({ min: 1000, max: 99999 });
+  let randomNumber = pl_1.faker.datatype.number({ min: 1000, max: 99999 });
   while (usedIdsArray.includes(randomNumber)) {
-    randomNumber = faker.datatype.number({ min: 1000, max: 99999 });
+    randomNumber = pl_1.faker.datatype.number({ min: 1000, max: 99999 });
   }
   usedIdsArray.push(randomNumber);
   return randomNumber;
 }
-
 function generateRandomFeatures() {
   const featuresArray = [];
-  const amountOfFeatures = faker.datatype.number({ min: 5, max: 9 });
+  const amountOfFeatures = pl_1.faker.datatype.number({ min: 5, max: 9 });
   for (let i = 0; i <= amountOfFeatures; i++) {
-    featuresArray.push(faker.lorem.word());
+    featuresArray.push(pl_1.faker.lorem.word());
   }
   return featuresArray;
 }
-
 function generateFakeVenuesData(numberOfVenuesToGenerate) {
   for (let i = 1; i <= numberOfVenuesToGenerate; i++) {
     const newVenue = {
       id: i,
       location: {
-        postalCode: faker.address.zipCode(),
-        name: faker.address.cityName(),
+        postalCode: pl_1.faker.address.zipCode(),
+        name: pl_1.faker.address.cityName(),
       },
-      pricePerNightInEur: faker.datatype.number({
+      pricePerNightInEur: pl_1.faker.datatype.number({
         min: 10,
         max: 35,
         precision: 0.01,
       }),
-      rating: faker.datatype.number({ min: 1, max: 5, precision: 0.1 }),
-      capacity: faker.datatype.number({ min: 1, max: 8 }),
-      name: faker.internet.domainWord(),
+      rating: pl_1.faker.datatype.number({ min: 1, max: 5, precision: 0.1 }),
+      capacity: pl_1.faker.datatype.number({ min: 1, max: 8 }),
+      name: pl_1.faker.internet.domainWord(),
       albumId: i,
-      landingImgUrl: faker.image.city(600, 600, true),
+      landingImgUrl: pl_1.faker.image.city(600, 600, true),
     };
     const newVenueDetails = {
       id: generateUniqueId(),
@@ -57,46 +54,46 @@ function generateFakeVenuesData(numberOfVenuesToGenerate) {
       capacity: newVenue.capacity,
       name: newVenue.name,
       albumId: newVenue.albumId,
-      description: faker.lorem.paragraph(3),
+      description: pl_1.faker.lorem.paragraph(3),
       features: generateRandomFeatures(),
       sleepingDetails: {
-        maxCapacity: faker.datatype.number({ min: 3, max: 8 }),
-        amountOfBeds: faker.datatype.number({ min: 1, max: 6 }),
+        maxCapacity: pl_1.faker.datatype.number({ min: 3, max: 8 }),
+        amountOfBeds: pl_1.faker.datatype.number({ min: 1, max: 6 }),
       },
       checkInHour: '4pm',
       checkOutHour: '10am',
-      distanceFromCityCenterInKM: faker.datatype.number({ min: 1, max: 15 }),
+      distanceFromCityCenterInKM: pl_1.faker.datatype.number({
+        min: 1,
+        max: 15,
+      }),
       contactDetails: {
-        phone: faker.phone.number(),
-        email: faker.internet.email(),
+        phone: pl_1.faker.phone.number(),
+        email: pl_1.faker.internet.email(),
       },
       landingImgUrl: newVenue.landingImgUrl,
       venueDescriptionImgUrls: {
-        1: faker.image.nature(600, 600, true),
-        2: faker.image.nature(600, 600, true),
-        3: faker.image.nature(600, 600, true),
-        4: faker.image.nature(600, 600, true),
-        5: faker.image.nature(600, 600, true),
-        6: faker.image.nature(600, 600, true),
-        7: faker.image.nature(600, 600, true),
+        1: pl_1.faker.image.nature(600, 600, true),
+        2: pl_1.faker.image.nature(600, 600, true),
+        3: pl_1.faker.image.nature(600, 600, true),
+        4: pl_1.faker.image.nature(600, 600, true),
+        5: pl_1.faker.image.nature(600, 600, true),
+        6: pl_1.faker.image.nature(600, 600, true),
+        7: pl_1.faker.image.nature(600, 600, true),
       },
     };
-    let addressToGenerate = faker.address.city();
+    let addressToGenerate = pl_1.faker.address.city();
     while (addressesCache.includes(addressToGenerate)) {
-      addressToGenerate = faker.address.city();
+      addressToGenerate = pl_1.faker.address.city();
     }
     generatedLocalizations.push({
-      label: faker.address.city(),
+      label: pl_1.faker.address.city(),
     });
     addressesCache.push(addressToGenerate);
-
     venueArray.push(newVenue);
     venueDetailsArray.push(newVenueDetails);
   }
 }
-
 generateFakeVenuesData(100);
-
 function createVenuesDetailsListWithPairedVenueId(venuesDetails) {
   let venueDetailsAndHisVenueId = {};
   venuesDetails.forEach((venue) => {
@@ -104,14 +101,11 @@ function createVenuesDetailsListWithPairedVenueId(venuesDetails) {
   });
   return venueDetailsAndHisVenueId;
 }
-
 const venuesDetailsAndHisVenueId =
   createVenuesDetailsListWithPairedVenueId(venueDetailsArray);
-
 const dataToPost = {
   venues: venueArray,
   venuesDetails: venuesDetailsAndHisVenueId,
   localizationOptions: generatedLocalizations,
 };
-
 console.log(JSON.stringify(dataToPost));
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: './build/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: '[name].[chunkhash].bundle.ts',
+    filename: '[name].[chunkhash].bundle.js',
   },
   mode: 'development',
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  plugins: [new HtmlWebpackPlugin({ template: './build/index.html' })],
   module: {
     rules: [
       {
